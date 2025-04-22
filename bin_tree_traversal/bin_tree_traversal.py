a899cd15be05e1fb420107fcb6cc3fd91fa3da2c
Kalmuk Yaropolk <yarko.kalmuk@gmail.com>
2025-04-22 23:24:42 +0300

    bin tree traversal done

--- bin_tree_traversal/bin_tree_traversal.py
+++ bin_tree_traversal/bin_tree_traversal.py
@@ -1,2 +1,29 @@
 """bin_tree_traversal"""
 
+class Node:
+    """Node in binary tree"""
+    def __init__(self, data, left, right):
+        self.data = data
+        self.left = left
+        self.right = right
+
+# Pre-order traversal
+def pre_order(node):
+    """RAB"""
+    if node is None:
+        return []
+    return [node.data] + pre_order(node.left) + pre_order(node.right)
+
+# In-order traversal
+def in_order(node):
+    """ARB"""
+    if node is None:
+        return []
+    return  in_order(node.left) + [node.data] + in_order(node.right)
+
+# Post-order traversal
+def post_order(node):
+    """ABR"""
+    if node is None:
+        return []
+    return  post_order(node.left) + post_order(node.right) + [node.data]
